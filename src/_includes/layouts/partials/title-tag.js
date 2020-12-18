ddab98810d999457bd318eb5f8770dee10a5563d
Bryce Wray <bwray@wraytx.com>
2020-12-18 14:30:41 -0600

    Deleting files used with .11ty.js templating

--- src/_includes/layouts/partials/title-tag.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (eleventyConfig) {
-
-  eleventyConfig.addShortcode('titleTag', function (data) {
-    return `
-    <title>${data.title} | brycewray.com</title>
-    `
-  })
-}
